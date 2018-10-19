dcf9c718b8430a579f8cba405a6878a387a77709
ahmedfouad <lionmoon60@yahoo.com>
2018-10-19 19:25:42 +0200

    rendering html

--- nodeJSBasics/server.js
+++ nodeJSBasics/server.js
@@ -1,9 +1,18 @@
 var http = require('http');
+var fs = require('fs');
 
 function onRequest(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello world');
-    response.end();
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write("file not found!");
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+    // response.end();
     
 }
 
