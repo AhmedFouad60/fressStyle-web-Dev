23049c9f80e0993182d2746c62543eeed9a72bef
ahmedfouad <lionmoon60@yahoo.com>
2018-10-20 07:06:39 +0200

    routing in nodeJS

--- nodeJSBasics/server.js
+++ nodeJSBasics/server.js
@@ -1,19 +1,26 @@
 var http = require('http');
-var fs = require('fs');
+var app=require('./app')
 
-function onRequest(request, response) {
+http.createServer(app.handelRequest).listen(8000);
+
+
+
+
+
+
+
+
+
+/*  //render html form the server
+    var fs = require('fs');
+    function onRequest(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
     fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write("file not found!");
-        } else {
-            response.write(data);
-        }
-        response.end();
+      if (error) {
+        response.writeHead(404); response.write("file not found!");
+      } else {
+        response.write(data);
+      } response.end();
     });
     // response.end();
-    
-}
-
-http.createServer(onRequest).listen(8000);
+    }*/
