b555937156bf886ea7ce11c0456883ed576b60eb
ahmedfouad <lionmoon60@yahoo.com>
2018-10-21 22:56:17 +0200

    get and post requests

--- nodeJSBasics/Handlebars/routes/index.js
+++ nodeJSBasics/Handlebars/routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express',condition:true,anyArray:[3,5,6] });
 });
+router.get('/test/:id', function(req, res, next){
+  res.render('test', { output: req.params.id });
+});
+router.post('/test/submit', function (req, res, next) {
+  var id = req.body.id;
+  res.redirect(`/test/${id}`);
+  
+});
 
 module.exports = router;
