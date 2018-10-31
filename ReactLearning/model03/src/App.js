57f12d11b9b9fdc1995ff8e12043233c1e47d4ed
ahmedfouad <lionmoon60@yahoo.com>
2018-10-31 21:21:45 +0200

    two way binding

--- ReactLearning/model03/src/App.js
+++ ReactLearning/model03/src/App.js
@@ -11,26 +11,51 @@ class App extends Component {
       {name:'Lionmoon',age:24}
     ]
   }
-  SwitchNameHandler = () => {
+  SwitchNameHandler = (newName) => {
     // console.log("was clicked");
     // Don't use thsis syntax :) this.state.Persons[0].name = "Ahmed Fouad";
     this.setState({
       Persons: [
-        {name:'Max',age:29},
-        {name:'FoushWare',age:23},
-        {name:'Stephanie',age:24}
-    ]})
+        { name: newName, age: 29 },
+        { name: 'FoushWare', age: 23 },
+        { name: 'Stephanie', age: 24 }
+      ]
+    });
+
+    
 
   }
+  nameChangedHandler = (event) => {
+    this.setState({
+      Persons: [
+        { name: event.target.value, age: 29 },
+        { name: 'FoushWare', age: 23 },
+        { name: 'Stephanie', age: 24 }
+      ]
+    });
+ 
+  }
+
   render() {
     return (
       <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working</p>
-        <button onClick={this.SwitchNameHandler}>Switch Name</button>
-        <Person name={this.state.Persons[0].name} age={this.state.Persons[0].age} />
+        <button onClick={ this.SwitchNameHandler.bind(this,'MAX!')}>Switch Name</button>
+        <Person
+          name={this.state.Persons[0].name}
+          age={this.state.Persons[0].age}
+          click={this.SwitchNameHandler}
+          changed={this.nameChangedHandler}
+
+        />
         <Person name={this.state.Persons[1].name} age={this.state.Persons[1].age} />
-        <Person name={this.state.Persons[2].name} age={this.state.Persons[2].age} />
+        <Person
+          name={this.state.Persons[2].name}
+          age={this.state.Persons[2].age}
+        
+        
+        />
   
       </div>
     );
--- ReactLearning/model03/src/Person/Person.js
+++ ReactLearning/model03/src/Person/Person.js
@@ -1,10 +1,11 @@
-import React  from 'react'
+import React from "react";
 
 const Person = (props) => {
   return (
     <div>
-      <p> I'm {props.name} and i'm {props.age}</p>
+      <p onClick={props.click}> I'm {props.name} and i'm {props.age}</p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
 
     </div>
   );
